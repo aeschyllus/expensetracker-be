1c8eaaaffee043e5d6cf8339ec836c2a6e7de40b
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-16 19:57:43 +0800

    chore: invoke main function in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -34,3 +34,12 @@ async function main() {
 
   console.log({ user, accounts, categories, subcategories });
 }
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
