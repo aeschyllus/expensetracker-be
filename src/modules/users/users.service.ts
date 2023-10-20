408311d101d35b927fbcb663d053913c7f9fdb94
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-20 19:08:42 +0800

    feat: add users module

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = await hash(
+      createUserDto.password,
+      Number(process.env.HASH_ROUNDS),
+    );
+    return this.prisma.user.create({ data: createUserDto });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findUniqueOrThrow({ where: { id } });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = await hash(
+        updateUserDto.password,
+        Number(process.env.HASH_ROUNDS),
+      );
+    }
+    return this.prisma.user.update({ where: { id }, data: updateUserDto });
+  }
+
+  remove(id: number) {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
