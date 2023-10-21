7d8b87c7a779e4d9e20c42328ef82d2d50d7f070
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-21 17:31:07 +0800

    chore: add happy path unit test for users module

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -2,13 +2,32 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+const usersArray = [
+  {
+    id: 1,
+    username: 'mlapada',
+    email: 'mlapada@mail.com',
+    createdAt: '2023-10-20T11:18:56.411Z',
+    updatedAt: '2023-10-20T11:18:56.411Z',
+  },
+];
+const oneUser = usersArray[0];
+
+const db = {
+  create: jest.fn().mockReturnValue(oneUser),
+  findAll: jest.fn().mockResolvedValue(usersArray),
+  findOne: jest.fn().mockResolvedValue(oneUser),
+  update: jest.fn().mockResolvedValue(oneUser),
+  remove: jest.fn().mockResolvedValue(oneUser),
+};
+
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{ provide: UsersService, useValue: db }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -16,5 +35,43 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should be able to create a user', () => {
+      expect(
+        controller.create({
+          username: 'mlapada',
+          email: 'mlapada@mail.com',
+          password: 'mlapada',
+        }),
+      ).resolves.toEqual(oneUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const users = await controller.findAll();
+      expect(users).toEqual(usersArray);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find one user', () => {
+      expect(controller.findOne(1)).resolves.toEqual(oneUser);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const user = await controller.update(1, { username: 'mlapada' });
+      expect(user).toEqual(oneUser);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a user', () => {
+      expect(controller.remove(1)).resolves.toEqual(oneUser);
+    });
   });
 });
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { hashSync } from 'bcrypt';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+const usersArray = [
+  {
+    id: 1,
+    username: 'mlapada',
+    email: 'mlapada@mail.com',
+    password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
+    createdAt: '2023-10-20T11:18:56.411Z',
+    updatedAt: '2023-10-20T11:18:56.411Z',
+  },
+];
+const oneUser = usersArray[0];
+
+const db = {
+  user: {
+    create: jest.fn().mockReturnValue(oneUser),
+    findMany: jest.fn().mockResolvedValue(usersArray),
+    findUniqueOrThrow: jest.fn().mockResolvedValue(oneUser),
+    update: jest.fn().mockResolvedValue(oneUser),
+    delete: jest.fn().mockResolvedValue(oneUser),
+  },
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, { provide: PrismaService, useValue: db }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +38,43 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should be able to create a user', () => {
+      expect(
+        service.create({
+          username: 'mlapada',
+          email: 'mlapada@mail.com',
+          password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
+        }),
+      ).resolves.toEqual(oneUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const users = await service.findAll();
+      expect(users).toEqual(usersArray);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find one user', () => {
+      expect(service.findOne(1)).resolves.toEqual(oneUser);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const user = await service.update(1, { username: 'mlapada' });
+      expect(user).toEqual(oneUser);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a user', () => {
+      expect(service.remove(1)).resolves.toEqual(oneUser);
+    });
   });
 });
