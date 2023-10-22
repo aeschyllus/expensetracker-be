d496d3029e44b11a79ffcd800866fb06205795cc
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-22 13:10:24 +0800

    chore: add negative path unit tests for users module

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,3 +1,8 @@
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { hashSync } from 'bcrypt';
 import { UsersService } from './users.service';
@@ -9,15 +14,15 @@ const usersArray = [
     username: 'mlapada',
     email: 'mlapada@mail.com',
     password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
-    createdAt: '2023-10-20T11:18:56.411Z',
-    updatedAt: '2023-10-20T11:18:56.411Z',
+    createdAt: new Date('2023-10-20T11:18:56.411Z'),
+    updatedAt: new Date('2023-10-20T11:18:56.411Z'),
   },
 ];
 const oneUser = usersArray[0];
 
 const db = {
   user: {
-    create: jest.fn().mockReturnValue(oneUser),
+    create: jest.fn().mockResolvedValue(oneUser),
     findMany: jest.fn().mockResolvedValue(usersArray),
     findUniqueOrThrow: jest.fn().mockResolvedValue(oneUser),
     update: jest.fn().mockResolvedValue(oneUser),
@@ -27,6 +32,7 @@ const db = {
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,6 +40,7 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
@@ -41,14 +48,43 @@ describe('UsersService', () => {
   });
 
   describe('create', () => {
-    it('should be able to create a user', () => {
-      expect(
-        service.create({
-          username: 'mlapada',
-          email: 'mlapada@mail.com',
-          password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
-        }),
-      ).resolves.toEqual(oneUser);
+    it('should be able to create a user', async () => {
+      const result = service.create({
+        username: 'mlapada',
+        email: 'mlapada@mail.com',
+        password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
+      });
+
+      await expect(result).resolves.toEqual(oneUser);
+    });
+
+    it('should throw ConflictException for duplicate username/email', async () => {
+      jest
+        .spyOn(prisma.user, 'create')
+        .mockRejectedValue(new ConflictException());
+
+      const result = service.create({
+        username: 'mlapada', // duplicate username
+        email: 'mlapada@mail.com', // duplicate email
+        password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
+      });
+
+      await expect(result).rejects.toThrowError(ConflictException);
+    });
+
+    // TODO: Document in Swagger
+    it('should throw BadRequestException for invalid inputs', async () => {
+      jest
+        .spyOn(prisma.user, 'create')
+        .mockRejectedValue(new BadRequestException());
+
+      const result = service.create({
+        username: '', // empty username
+        email: 'mlapada@mail.com',
+        password: hashSync('mlapada', Number(process.env.HASH_ROUNDS)),
+      });
+
+      await expect(result).rejects.toThrowError(BadRequestException);
     });
   });
 
@@ -60,8 +96,19 @@ describe('UsersService', () => {
   });
 
   describe('findOne', () => {
-    it('should find one user', () => {
-      expect(service.findOne(1)).resolves.toEqual(oneUser);
+    it('should find one user', async () => {
+      await expect(service.findOne(1)).resolves.toEqual(oneUser);
+    });
+
+    // TODO: Document in Swagger
+    it('should throw NotFoundException for non-existing user', async () => {
+      jest
+        .spyOn(prisma.user, 'findUniqueOrThrow')
+        .mockRejectedValue(new NotFoundException());
+
+      const result = service.findOne(2); // non-existing ID
+
+      await expect(result).rejects.toThrowError(NotFoundException);
     });
   });
 
@@ -70,11 +117,54 @@ describe('UsersService', () => {
       const user = await service.update(1, { username: 'mlapada' });
       expect(user).toEqual(oneUser);
     });
+
+    it('should throw NotFoundException for non-existing user', async () => {
+      jest
+        .spyOn(prisma.user, 'update')
+        .mockRejectedValue(new NotFoundException());
+
+      const result = service.update(2, { username: 'mlapada' }); // non-existing ID
+
+      await expect(result).rejects.toThrowError(NotFoundException);
+    });
+
+    it('should throw ConflictException for duplicate username/email', async () => {
+      jest
+        .spyOn(prisma.user, 'update')
+        .mockRejectedValue(new ConflictException());
+
+      const result = service.update(1, { username: 'mlapada' }); // duplicate username
+
+      await expect(result).rejects.toThrowError(ConflictException);
+    });
+
+    // TODO: Document in Swagger
+    it('should throw BadRequestException for invalid inputs', async () => {
+      jest
+        .spyOn(prisma.user, 'update')
+        .mockRejectedValue(new BadRequestException());
+
+      const result = service.update(1, {
+        username: '', // empty username
+      });
+
+      await expect(result).rejects.toThrowError(BadRequestException);
+    });
   });
 
   describe('remove', () => {
     it('should delete a user', () => {
       expect(service.remove(1)).resolves.toEqual(oneUser);
+    });
+
+    it('should throw NotFoundException for non-existing user', async () => {
+      jest
+        .spyOn(prisma.user, 'delete')
+        .mockRejectedValue(new NotFoundException());
+
+      const result = service.remove(2); // non-existing ID
+
+      await expect(result).rejects.toThrowError(NotFoundException);
     });
   });
 });
