feb3309cda05778c4a15e1d7eefdf5fc02622a9a
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-22 21:14:53 +0800

    chore: add app modules in test coverage

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppModule } from './app.module';
 
 describe('AppController', () => {
+  let appModule: AppModule;
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
+    appModule = app.get<AppModule>(AppModule);
     appController = app.get<AppController>(AppController);
+  });
+
+  it('should be defined', () => {
+    expect(appModule).toBeDefined();
   });
 
   describe('root', () => {
